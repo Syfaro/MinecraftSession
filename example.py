7390a9a7f6333c0af873fc5584334989ba3dcec0
Syfaro Warraw <sy@syfaro.net>
2013-08-18 10:33:57 -0500

    Update example.py

--- example.py
+++ example.py
@@ -4,20 +4,20 @@ mcsession = minecraftsession.MinecraftSession()
 
 # Logs in and creates session information
 if not mcsession.login('email', 'password', 'something random'):
-	print 'Wrong email or password'
+  print 'Wrong email or password'
 
 # Checks if session information is accurate
 if not mcsession.checkSession():
-	print 'Invalid session information'
+  print 'Invalid session information'
 
 # Checks if session information is accurate if you aren't using the login method
-if not mcsession.checkSession('token:asdfasdf:asdfasdf'):
-	print 'Invalid session information / client information'
+if not mcsession.checkSession('asdfasdf'):
+  print 'Invalid session information'
 
 # Refreshes session information if you used login with a client name (something random)
 if not mcsession.refreshSession():
-	print 'Session timed out'
+  print 'Session timed out'
 
 # Refreshes session information if you didn't use login
-if not mcsession.refreshSession('token:asdfasdf:asdfasdf', 'something random that you logged in with'):
-	print 'Invalid information'
+if not mcsession.refreshSession('asdfasdf', 'something random that you logged in with'):
+  print 'Invalid information'
